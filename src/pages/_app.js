247c5432a39fce8ab14df6b27ac35af2a9fc81c0
Christian Hall <jchristianhall@me.com>
2020-01-26 22:50:16 -0600

    Migrate to NextJS (#2)
    * Start from scratch with NextJS
    
    * Add GoSquared script
    
    * Add base design for Home
    
    * Design adjustments
    
    * Add social links
    
    * Setup for now deployment

---
+++ src/pages/_app.js
@@ -0,0 +1,30 @@
+import "./global.css"
+import Head from "next/head"
+
+export default function App({ Component, pageProps }) {
+  const gosquaredSnippet = `!function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+  insertBefore(d,q)}(window,document,'script','_gs');
+
+  _gs('GSN-329410-F');
+  _gs('set', 'anonymizeIP', true);`
+
+  return (
+    <>
+      <Head>
+        {process.env.NODE_ENV === "production" && (
+          <script
+            key="gosquared"
+            dangerouslySetInnerHTML={{ __html: gosquaredSnippet }}
+          />
+        )}
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,40 @@
-import ComingSoon from "../components/ComingSoon"
-import Layout from "../components/layout"
-import React from "react"
-import SEO from "../components/seo"
+function Home() {
+  return (
+    <main className="Home">
+      <div className="HomeBackground">
+        <div className="HomeBackground-element HomeBackground-top1"></div>
+        <div className="HomeBackground-element HomeBackground-top2"></div>
+        <div className="HomeBackground-element HomeBackground-bottom1"></div>
+        <div className="HomeBackground-element HomeBackground-bottom2"></div>
+      </div>
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Christian Hall | Coming Soon" />
-    <ComingSoon />
-  </Layout>
-)
+      <div className="Home-content">
+        <h1 className="Home-name">Christian Hall</h1>
+        <h2 className="Home-byline">Software Engineer</h2>
 
-export default IndexPage
+        <p className="Home-bio">
+          Christian is a software engineer who cares deeply for usable designs
+          and sustainable development.
+        </p>
+
+        <div className="Home-links">
+          <a href="/Christian Hall Resume.pdf" className="Home-link">
+            Resume
+          </a>
+          <a href="https://twitter.com/jchristianhall" className="Home-link">
+            Twitter
+          </a>
+          <a href="https://github.com/jchristianhall" className="Home-link">
+            GitHub
+          </a>
+        </div>
+
+        <footer className="Home-footer">
+          Crafted in Tennessee. &copy; {new Date().getFullYear()}
+        </footer>
+      </div>
+    </main>
+  )
+}
+
+export default Home
